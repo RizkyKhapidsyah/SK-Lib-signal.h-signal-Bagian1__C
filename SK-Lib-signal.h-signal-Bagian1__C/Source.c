a398f25783c3b974c1fec6097cb2c332aa98c8f8
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-09-25 09:57:58 +0700

    Add project files.

---
+++ SK-Lib-signal.h-signal-Bagian1__C/Source.c
@@ -0,0 +1,24 @@
+#include <stdio.h>      /* printf */
+#include <signal.h>     /* signal, raise, sig_atomic_t */
+#include <conio.h>
+
+sig_atomic_t signaled = 0;
+
+void my_handler(int param) {
+	signaled = 1;
+}
+
+int main() {
+	void (*prev_handler)(int);
+
+	prev_handler = signal(SIGINT, my_handler);
+
+	/* ... */
+	raise(SIGINT);
+	/* ... */
+
+	printf("signaled is %d.\n", signaled);
+	
+	_getch();
+	return 0;
+}
